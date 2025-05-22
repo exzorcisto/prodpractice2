44f04292298c45ca7443dd07d88bae07a0b2e036
exzorcisto <yimov1804@gmail.com>
2025-05-22 22:47:35 +0700

    commit-9-1-save-dev

--- prac2/server/Server.js
+++ prac2/server/Server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs'); // file system
+const path = require('path'); // forming the correct path
 
 const PORT = 3000
 
@@ -8,8 +10,31 @@ const server = http.createServer((req, res) => {
 
     res.setHeader('Content-type', 'text/html')
 
-    res.write('<h1>Hello 1ISP!</h1>')
-    res.end();
+    const createPath = (page) => path.resolve('src', 'pages', `${page}`, `${page}.html`);
+
+    let basePath = '';
+    switch(req.url) {
+        case '/':
+            basePath = createPath('TheEncoder');
+            res.statusCode = 200;
+            break;
+        default:
+            basePath = createPath('Error');
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(basePath, (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end();
+        }
+        else {
+            res.write(data);
+            res.end();
+        }
+    })
+    
 });
 
 server.listen(3000, 'localhost', (error) => {
